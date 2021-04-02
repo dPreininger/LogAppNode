7f7415f8843db0b70fa3ee8d8ac1f644bf297610
David Preininger <goljatpre@gmail.com>
2021-04-02 11:45:19 +0200

    basic api functionality

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const path = require('path');
 const exphbs  = require('express-handlebars');
 const port = 8080;
 
+// routes
+const apiRouter = require("./api/apiRoutes");
+
 server.engine('.hbs', exphbs({extname: '.hbs'}));
 server.set('view engine', '.hbs');
 
 server.use(express.static(path.join(__dirname, 'static')));
 
+server.use('/api', apiRouter);
+
 server.get('/', (req, res) => {
-    res.render('domov');
+    res.render('domov', );
 })
 
 
